4f3c5c449becbb0cb114aab8682d646b028721ac
377312117 <377312117@qq.com>
2022-02-26 12:25:11 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/m/v1/zzx/gocode
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/m/v1/zzx/gocode/interface_test"
+)
+
+func test (an interfacetest.Animal){
+	fmt.Printf("animals:%s, %s", an.Name(), an.Fly())
+}
+
+func main () {
+	fish1 :=interfacetest.Fish{}
+	test(fish1)
+}
